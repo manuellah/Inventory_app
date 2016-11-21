1cfc3bf926786214de8ad117cf555ca3efddf3ef
manuellah <emmanumuthui@gmail.com>
2016-11-21 23:54:56 +0300

    Database models.py added.

---
+++ main.py
@@ -0,0 +1,13 @@
+import click
+from pyfiglet import figlet_format
+from cmd import Cmd
+from inventory_app import inventory
+
+
+class main(Cmd):
+    def do_item_add(self, args):
+        pass
+    
+    def do_item_remove(self, args):
+        pass
+     def 
