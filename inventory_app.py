c978d4eee091d39e44a261cc6fc46cd3baa74f06
manuellah <emmanumuthui@gmail.com>
2016-11-21 16:36:36 +0300

    Skeleton of my inventory class

---
+++ inventory_app.py
@@ -0,0 +1,27 @@
+class inventory(object):
+    def __init__(self, inventory):
+        self.inventory = inventory
+        
+    def item_add(self):
+        pass
+    
+    def item_remove(self):
+        pass
+    
+    def item_list(self):
+        pass
+    
+    def item_check_out(self):
+        pass
+    
+    def item_check_in(self):
+        pass
+    
+    def item_view(self):
+        pass
+    
+    def assetvalue(self):
+        pass
+    
+    def item_search(self, item):
+        pass
