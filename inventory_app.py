c82c93bd3e2f5209e31c4432f38947e3d66ef549
manuellah <emmanumuthui@gmail.com>
2016-11-22 20:13:36 +0300

    Interaction with the data base to query data, add and delete

--- inventory_app.py
+++ inventory_app.py
@@ -1,21 +1,50 @@
+from models import  Asset, log, Session
+from datetime import datetime
+
 class inventory(object):
     def __init__(self, inventory):
         self.inventory = inventory
+        self.ses = Session()
         
     def item_add(self):
+        print "\t\t\tItem Details"
         
-    
-    def item_remove(self):
-        pass
-    
+        item_name = raw_input("\t\tName : ")
+        item_description = raw_input('\t\tDescription : ')
+        item_quantity = raw_input('\t\tQuantity : ')
+        cost_per_item = raw_input("\t\tCost Per Item : ")
+        date_added = datetime.utcnow()
+        item_status = True
+        
+        self.ses.add(Asset(item_name, item_description, item_quantity, 
+                    cost_per_item, date_added,  item_status))
+        self.ses.commit()
+        
+        print "\t\t\tSuccessfully Added"
+        
+    def item_remove(self, item_id):
+        self.ses.query(Asset.AssetId).filter(Asset.AssetId == item_id).delete()
+        self.ses.commit()
+        
+        
     def item_list(self):
-        pass
-    
-    def item_check_out(self):
-        pass
-    
-    def item_check_in(self):
-        pass
+        mydata = []
+        rs = self.ses.query(Asset).all()
+        self.ses.commit()
+        for item in rs:
+            mydata.append([item.AssetId, item.Item_name, item.Item_description,
+             item.Item_amount_available, item.Cost_per_item, item.Date_added,
+             item.Item_status])
+            
+        return mydata
+ 
+    def item_check_out(self, item_id):
+        self.ses.add(log(datetime.utcnow(), None, item_id))
+        self.ses.commit()
+    def item_check_in(self, item_id):
+        the_item = self.ses.query( log.Check_in_date).filter_by(log.AssetId==item_id).update()
+        the_item.Check_in_date = datetime.utcnow()
+        self.ses.commit()
     
     def item_view(self):
         pass
@@ -26,4 +55,15 @@ class inventory(object):
     def item_search(self, item):
         pass
     
+    def update_log(self, item_id):
+        ads=Asset('Manu','Bootcamp process', 400, 500,100, datetime.utcnow(), True)
+        ads = log(datetime.utcnow(), datetime.utcnow(), 1)
+        
+        self.ses.add(ads)
+        self.ses.commit()   
+
+        
+asd = inventory('my inventory')
+asd.item_check_in(1)
+
     
--- models.py
+++ models.py
@@ -2,36 +2,35 @@ from sqlalchemy import create_engine, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Float, DateTime, Boolean
 from sqlalchemy.orm import sessionmaker, relationship
-from datetime import datetime
+
 
 eng = create_engine('sqlite:///inventory.db')
 Base = declarative_base()
+Base.metadata.bind = eng 
 Session = sessionmaker(bind=eng)
-ses = Session()   
- 
+
+
 class Asset(Base):
     __tablename__ = "Assets"
  
     AssetId = Column(Integer, primary_key=True)
     Item_name = Column(String) 
     Item_description = Column(String)
-    Item_total = Column(Integer)
-    Item_available_ammount = Column(Integer)
-    Item_cost_per_item = Column(Float)
-    Item_date_added = Column(DateTime)
+    Item_amount_available = Column(Integer)
+    Cost_per_item = Column(Float)
+    Date_added = Column(DateTime)
     Item_status = Column(Boolean)
 
     logs = relationship("log")
 
-    def __init__(self, Item_name, Item_description, Item_total, Item_available_ammount,
-    			Item_cost_per_item, Item_date_added, Item_status):
+    def __init__(self, Item_name, Item_description, Item_amount_available,
+                 Cost_per_item, Item_date_added, Item_status):
         
         self.Item_name = Item_name
         self.Item_description = Item_description
-        self.Item_total = Item_total
-        self.Item_available_ammount = Item_available_ammount
-        self.Item_cost_per_item = Item_cost_per_item
-        self.Item_date_added = Item_date_added
+        self.Item_amount_available = Item_amount_available
+        self.Cost_per_item = Cost_per_item
+        self.Date_added = Item_date_added
         self.Item_status = Item_status
 
 class log(Base):
@@ -45,14 +44,15 @@ class log(Base):
     Assets = relationship("Asset")   
 
     def __init__(self, Check_out_date, Check_in_date, AssetId):
-    	 self.Check_out_date =  Check_out_date     
-    	 self.Check_in_date =   Check_in_date  
-    	 self.AssetId = AssetId           
-         
-Base.metadata.bind = eng        
+        self.Check_out_date =  Check_out_date 
+        self.AssetId = AssetId 
+        self.Check_in_date =   Check_in_date
+            
 Base.metadata.create_all() 
 
 #ads=Asset('Manu','Bootcamp process', 400, 500,100, datetime.utcnow(), True)
-ads = log(datetime.utcnow(), datetime.utcnow(), 1)
-ses.add(ads)
-ses.commit()   
+#ads = log(datetime.utcnow(), datetime.utcnow(), 1)
+#ses.add(ads)
+
+#ses= Session()
+#print ses.query(Asset.Item_name, Asset.Item_description).all()
